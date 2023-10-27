448861b08dd99b3839b10ea597f0a765de1833b6
Aeo Zatoichi Bax <109118450+im-aeo@users.noreply.github.com>
2023-10-27 09:19:44 -0400

    Update beta.go

--- Items/beta.go
+++ Items/beta.go
@@ -9,17 +9,17 @@ import (
 )
 
 var (
-	eye           = V(2.5, 1.5, 10) // 9.5,9.5,40 if render is false
+	eye           = V(2.3, 1.6, 7) // 9.5,9.5,40 if render is false
 	center        = V(0, 0, 0) // 0,2,0 if render is false
 	up            = V(0, 5, 0) // 0,3,0 if render is false
 	Dimentions    = 512
 	CameraScale   = 1  // set to 4 or 5 for production, 2 or 3 for testing and 1 for obj formating
-	light         = V(12, 16, 25).Normalize()
+	light         = V(16, 22, 25).Normalize()
 	fovy   		  = 22.5 // 1.5 if render is false
 	near  		  = 1.0 // 2 if render is false
 	far    		  = 1000.0
-	color  		  = "#ffffff"
-	Amb           = "#606060"
+	color  		  = "#828282"
+	Amb           = "#d4d4d4"
 	cdnDirectory  = "/var/www/cdn"
 )
 
@@ -38,7 +38,7 @@ func main() {
 	objects := []*Object{
     &Object{
         Mesh:    LoadObject(filepath.Join(cdnDirectory, "/uploads/"+hash+".obj")), // Adjust the path as needed
-        Texture: faceTexture,
+        Texture: filepath.Join(cdnDirectory, "uploads", *hash+".png"),
         Color:   HexColor("#ffffff"), // You can set the color as needed
     }
 	}
@@ -54,29 +54,10 @@ func main() {
     }
     objects = append(objects, faceObject)
 	}
-
-	// Render and append the hat objects
-	hatObjects := RenderHats(*hat1, *hat2, *hat3, *hat4, *hat5, *hat6)
-	objects = append(objects, hatObjects...)
   
 	path := filepath.Join(cdnDirectory, "thumbnails", *hash+".png")
   GenerateScene(true, path, objects, eye, center, up, fovy, Dimentions, CameraScale, light, Amb, "ffffff", near, far)
 	fmt.Println("Item Thumbnail Rendered In:", time.Since(start), "at", path)
-}
-func RenderHats(hats ...string) []*Object {
-    var objects []*Object
-
-    for _, hat := range hats {
-        if hat != "none" {
-            obj := &Object{
-                Mesh:    LoadObject(filepath.Join(cdnDirectory, "/uploads/"+hat+".obj")),
-                Texture: LoadTexture(filepath.Join(cdnDirectory, "/uploads/"+hat+".png")),
-            }
-            objects = append(objects, obj)
-        }
-    }
-
-    return objects
 }
 func AddFace(facePath string) Texture {
     var face Texture
@@ -84,7 +65,7 @@ func AddFace(facePath string) Texture {
     if facePath != "none" {
         face = LoadTexture(filepath.Join(cdnDirectory, "/uploads/"+facePath+".png"))
     } else {
-        face = LoadTextureFromURL("https://cdn.discordapp.com/attachments/883044424903442432/1145691010345730188/face.png")
+       return nil
     }
 
     return face
